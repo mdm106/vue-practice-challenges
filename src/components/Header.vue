c72398fb32b8cf7837109670b1d1919a14a1d55f
mdm106 <mads.mcmahon@googlemail.com>
2021-03-24 12:07:19 +0000

    Creating Header component imported into Hello World main component

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{ heading }}</h1>
+</template>
+
+<script>
+
+export default {
+  name: 'Header',
+  props: {
+    heading: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  margin: 40px 0 0;
+}
+</style>
