28018ef33382bbf58223514fefd0676b403a80e6
mdm106 <mads.mcmahon@googlemail.com>
2021-03-24 12:14:00 +0000

    Adding square component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,18 +2,21 @@
   <div>
     <Header heading="This is a test" />
     <Paragraph text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quamquam tu hanc copiosiorem etiam soles dicere. Ubi ut eam caperet aut quando? Videmus igitur ut conquiescere ne infantes quidem possint. Magna laus. Bonum patria: miserum exilium. Sed tu istuc dixti bene Latine, parum plane. Duo Reges: constructio interrete. Ergo hoc quidem apparet, nos ad agendum esse natos" />
+    <Square />
   </div>
 </template>
 
 <script>
 import Header from './Header.vue'
 import Paragraph from './Paragraph.vue'
+import Square from './Square.vue'
 
 export default {
   name: 'HelloWorld',
   components: {
     Header,
-    Paragraph
+    Paragraph,
+    Square
   }
 }
 </script>
---
+++ src/components/Square.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Square',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  div {
+    width: 200px;
+    height: 200px;
+    background-color: red;
+    margin: auto;
+  }
+</style>
